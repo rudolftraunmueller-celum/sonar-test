5e8d6eb8f3e82dcfa97267974cb72c3234862e40
Rudolf Traunmüller <rudolf.traunmller@celum.com>
2019-01-15 08:01:16 +0100

    added sonar to test app

---
+++ src/main/java/com/celum/sonartest/SonartestApplication.java
@@ -0,0 +1,14 @@
+package com.celum.sonartest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonartestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SonartestApplication.class, args);
+  }
+
+}
+
---
+++ src/test/java/com/celum/sonartest/SonartestApplicationTests.java
@@ -0,0 +1,17 @@
+package com.celum.sonartest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SonartestApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
+
