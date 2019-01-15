3e326f5bd92ac8ef601d18d239e4fb67fc93ea05
Rudolf Traunmüller <rudolf.traunmller@celum.com>
2019-01-15 08:25:21 +0100

    added test for sonar coverage check

---
+++ src/main/java/com/celum/sonartest/TestUtil.java
@@ -0,0 +1,12 @@
+package com.celum.sonartest;
+
+public class TestUtil {
+
+  public void complexCode() {
+    String[] test = new String[]{"test", "test2", "test3"};
+    for (String s : test) {
+      System.out.println(s);
+    }
+  }
+
+}
---
+++ src/test/java/com/celum/sonartest/TestUtilTest.java
@@ -0,0 +1,11 @@
+package com.celum.sonartest;
+
+import org.junit.Test;
+
+public class TestUtilTest {
+
+  @Test
+  public void complexCode() {
+    new TestUtil().complexCode();
+  }
+}
